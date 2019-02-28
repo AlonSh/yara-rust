8620f1880cc9053c47667aaa53656ecd9008a485
Alon Shenkler <alonshenk@gmail.com>
2019-02-28 13:44:27 +0200

    Add simple usage tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ extern crate failure;
 #[macro_use]
 extern crate lazy_static;
 extern crate yara_sys;
+#[cfg(test)]
+extern crate tempdir;
 
 mod compiler;
 mod internals;
@@ -54,5 +56,105 @@ impl Yara {
 impl Drop for Yara {
     fn drop(&mut self) {
         internals::finalize().expect("Expect correct Yara finalization");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+    use tempdir::TempDir;
+
+    static FILE_1_CONTENT: &[u8; 5] = b"Test1";
+
+    fn root_folder() -> TempDir {
+        let tmp_dir = TempDir::new("test").unwrap();
+        tmp_dir
+    }
+
+    fn file_1(root: &TempDir) -> PathBuf {
+        let root_folder_1 = root.path().join("folder_1");
+
+        fs::create_dir_all(&root_folder_1).expect(&format!(
+            "Failed to create dir {:#?}",
+            &root_folder_1.to_str().unwrap()
+        ));
+
+        let file_11 = root_folder_1.join("test_11.txt");
+
+        let mut f = File::create(&file_11).unwrap();
+        f.write(FILE_1_CONTENT).unwrap();
+
+        file_11
+    }
+
+    #[test]
+    fn test_scan_file_with_non_matching_rule() {
+        let root = root_folder();
+        let file_1 = file_1(&root);
+
+        let mut yara = Yara::create().unwrap();
+        let mut compiler = yara.new_compiler().unwrap();
+
+        match compiler.add_rules_str_with_namespace(
+            "rule not_match {
+          strings:
+            $text = \"RANDOM TEXT\" nocase
+          condition:
+            $text
+        }",
+            "yara-scan",
+        ) {
+            Ok(_) => {}
+            Err(e) => assert!(false),
+        };
+
+        let mut rules = compiler
+            .compile_rules()
+            .expect("Should have compiled rules");
+
+        let scan_results = rules.scan_file(file_1, 0);
+        assert!(scan_results.is_ok());
+        let scan_results = scan_results.unwrap();
+        assert_eq!(scan_results.len(), 0);
+    }
+
+    #[test]
+    fn test_scan_file_with_matching_rule() {
+        let root = root_folder();
+        let file_1 = file_1(&root);
+
+        let mut yara = Yara::create().unwrap();
+        let mut compiler = yara.new_compiler().unwrap();
+
+        match compiler.add_rules_str_with_namespace(
+            "rule matches_the_test_file {
+            strings:
+            $text = \"Test1\" nocase
+  condition:
+    $text
+}",
+            "yara-scan",
+        ) {
+            Ok(_) => {}
+            Err(e) => assert!(false),
+        };
+
+        let mut rules = compiler
+            .compile_rules()
+            .expect("Should have compiled rules");
+
+        let scan_results = rules.scan_file(file_1, 0);
+
+        println!("{:#?}", scan_results);
+
+        assert!(scan_results.is_ok());
+        let scan_results = scan_results.unwrap();
+        assert_eq!(scan_results.len(), 1);
+        let matching_rule = &scan_results[0];
+        assert_eq!(matching_rule.identifier, "matches_the_test_file");
     }
 }
