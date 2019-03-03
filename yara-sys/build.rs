2c0e2f3df7021f3ab26a03e201a781893b803d7a
Alon Shenkler <alonshenk@gmail.com>
2019-03-03 15:05:56 +0200

    Support linux with statically linked yara (WIP)

--- yara-sys/build.rs
+++ yara-sys/build.rs
@@ -2,11 +2,28 @@
 
 fn main() {
     // Tell cargo to tell rustc to link statically to the given yara lib.
-    println!("cargo:rustc-link-search=./external");
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-search=./external/macos");
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-search=./external/linux");
+
     println!("cargo:rustc-link-lib=static=yara");
 
     build::add_bindings();
 }
+//
+//mod build {
+//    use std::env;
+//    use std::fs;
+//    use std::path::PathBuf;
+//
+//    pub fn add_bindings() {
+//        let out_dir = env::var("OUT_DIR").unwrap();
+//        let out_path = PathBuf::from(out_dir).join("bindings.rs");
+//        fs::copy("bindings/yara-3.9.rs", out_path)
+//            .expect("Could not copy bindings to output directory");
+//    }
+//}
 
 mod build {
     extern crate bindgen;
